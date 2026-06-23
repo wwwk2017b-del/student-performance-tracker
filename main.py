4ee13702bc524b0cf346a5ca1f5752a7d30d40f3
Abhishek <www.k2017b@gmail.com>
2026-06-23 15:35:25 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+print("=" * 50)
+print(" CodTech IT Solutions — ML Internship")
+print(" Intern   : Abhishek Prasad")
+print(" Intern ID: CITS5099")
+print(" Project  : Student Performance Tracker")
+print("=" * 50)
+
+import generate_data
+import visualize
+import model
+
+print("\n✅ All tasks completed successfully!")
